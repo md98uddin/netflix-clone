38cf6bee0aaa36c05719081751455416ad1b53a2
MD R UDDIN <47330880+md98uddin@users.noreply.github.com>
2021-08-19 21:52:23 -0400

    dynamic navigation for main screen, desktop and mobile

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "../axios";
 import requests from "../requests";
 import "../css/Banner.css";
+import NavLoggedIn from "./shared/NavLoggedIn";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -24,29 +25,32 @@ function Banner() {
   }
 
   return (
-    <header
-      className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-        backgroundPosition: "center center",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {" "}
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button">Play</button>
-          <button className="banner__button">My List</button>
+    <>
+      <NavLoggedIn />
+      <header
+        className="banner"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+          backgroundPosition: "center center",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+            {" "}
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+          </h1>
         </div>
-        <h1 className="banner__description">
-          {truncate(movie?.overview, 150)}
-        </h1>
-      </div>
+      </header>
       <div className="banner--fadeBottom"></div>
-    </header>
+    </>
   );
 }
 
---
+++ src/components/shared/NavLoggedIn.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { FaHamburger } from "react-icons/fa";
+import "../../css/NavLoggedIn.css";
+
+const NavLoggedIn = () => {
+  const [isOpen, setToggle] = useState(false);
+
+  const toggle = () => setToggle((prevState) => !prevState);
+  return (
+    <>
+      <div className="nav__container">
+        <img
+          src={require("../../assets/images/netflix_logo.png").default}
+          alt="netflix__img__header"
+          role="button"
+        />
+        <div className="nav__list__container lg__screen">
+          <ul className="nav__list">
+            {["Home", "TV Shows", "Movies", "New & Popular", "My List"].map(
+              (category) => (
+                <Link
+                  to={`/view/${category.toLowerCase()}`}
+                  className="list__link"
+                >
+                  <li key={category} className="list-center" role="button">
+                    {category}
+                  </li>
+                </Link>
+              )
+            )}
+          </ul>
+        </div>
+        <div className="nav__dropdown__container sm__screen">
+          <Dropdown isOpen={isOpen} toggle={toggle}>
+            <DropdownToggle style={{ backgroundColor: "black" }}>
+              <FaHamburger
+                style={{
+                  backgroundColor: "black",
+                  color: "grey",
+                  border: "none",
+                }}
+                className=""
+                role="button"
+              />
+            </DropdownToggle>
+            <DropdownMenu
+              style={{
+                color: "white",
+                backgroundColor: "rgb(10,10,10, 0.9)",
+                marginTop: "0.6em",
+              }}
+            >
+              <DropdownItem header>Discover</DropdownItem>
+              {["Home", "TV Shows", "Movies", "New & Popular", "My List"].map(
+                (category) => (
+                  <Link
+                    to={
+                      category === "Home"
+                        ? "/"
+                        : `/view/${category.toLowerCase()}`
+                    }
+                    className="list__link"
+                  >
+                    <li key={category} className="list-left" role="button">
+                      {category}
+                    </li>
+                  </Link>
+                )
+              )}
+              <DropdownItem header>Account</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavLoggedIn;
